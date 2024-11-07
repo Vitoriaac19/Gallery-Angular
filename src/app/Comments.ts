54e0ecda4330f4c7c9bc2e025affe6b822b25b85
Vitoriaac19 <vitoriaac19@gmail.com>
2024-11-07 16:29:41 +0000

    reactive forms

---
+++ src/app/Comments.ts
@@ -0,0 +1,8 @@
+export interface Comments{
+    id?:string;
+    text:string;
+    username:string;
+    momentId:number;
+    create_at?:string;
+    updated_at?:string;
+}
--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,14 +1,42 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-moment-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './moment-form.component.html',
   styleUrl: './moment-form.component.css'
 })
 export class MomentFormComponent {
   @Input() btnText!:string;
 
+  momentForm!: FormGroup
+
+  ngOnInit(): void{ //inicializando coisas do angular pra inicializar momentForm
+    this.momentForm = new FormGroup({
+      id: new FormControl(""),
+      title: new FormControl("", [Validators.required]),
+      description: new FormControl("", [Validators.required]),
+      image: new FormControl("")
+    })
+  }
+
+  get title(){
+    return this.momentForm.get("title")!;
+  }
+
+  get description(){
+    return this.momentForm.get("description")!;
+  }
+
+  submit() {
+    if (this.momentForm.invalid){
+      return
+    }
+    console.log("enviado")
+  }
 
 }
